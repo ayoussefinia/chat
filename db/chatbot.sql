286b1a65d1ad375da91daf63fd355d1fe3b89427
Alex <alex.youssefinia@gmail.com>
2025-09-12 13:47:26 -0500

    update backend dockerfile

--- db/chatbot.sql
+++ db/chatbot.sql
@@ -1,6 +1,12 @@
--- Active: 1757557610145@@172.20.0.2@5432@chatbot_db@public
+-- Active: 1756880765923@@dpg-d2rrovp5pdvs73eckql0-a.ohio-postgres.render.com@5432@todo_huws@public
 
 CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  email TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS chat_users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
@@ -13,3 +19,10 @@ VALUES (
   '$2a$10$E6mJ9O.Yeje8TokTs6UjKOrX.YbAELdpXfUdj.L.0HfCygsVcLcze'
 )
 ON CONFLICT (email) DO NOTHING;
+
+-- Insert a dummy user with a bcrypt password (matches your earlier one)
+INSERT INTO chat_users (email, password)
+VALUES (
+  'test@example.com',
+  '$2a$10$E6mJ9O.Yeje8TokTs6UjKOrX.YbAELdpXfUdj.L.0HfCygsVcLcze'
+)
